46e4ac64808efc98671928e17ae7237aed787dc7
Augusto Martinez <acm_asd@hotmail.com>
2020-07-29 15:22:44 -0600

    Update table_setup.sql

--- app/config/table_setup.sql
+++ app/config/table_setup.sql
@@ -7,3 +7,9 @@ CREATE TABLE IF NOT EXISTS eom_services (
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 ) ENGINE = MyISAM DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci AUTO_INCREMENT = 1;
+
+
+INSERT INTO eom_services (id, services, service_value, created_at, updated_at) VALUES
+(1, 'Painting', '20', '2020-07-21 00:30:07', '2020-07-21 00:30:07'),
+(2, 'Drywall', '45', '2020-07-21 00:30:07', '2020-07-21 00:30:07'),
+(3, 'Sheetrock', '33.05', '2020-07-21 00:30:07', '2020-07-21 00:30:07');
